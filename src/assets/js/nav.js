9140b15b783915e09e5459bd38d9bd54dc2ed4a0
Lala H <smartyboots@users.noreply.github.com>
2024-10-09 13:44:04 +1300

    refine nav js
    converted to arrow functions

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -5,27 +5,24 @@ const navbarMenu = document.querySelector("#navigation #navbar-menu");
 const navBarMenuItems = navbarMenu.querySelectorAll("ul li a");
 const hamburgerMenu = document.querySelector("#navigation .hamburger-menu");
 
-function openNav() {
+const openNav = () => {
   hamburgerMenu.setAttribute("aria-expanded", true);
   hamburgerMenu.classList.add("clicked");
   navbarMenu.classList.add("open");
 }
-function closeNav() {
+const closeNav = () => {
   hamburgerMenu.setAttribute("aria-expanded", false);
     hamburgerMenu.classList.remove("clicked");
     navbarMenu.classList.remove("open");
 }
 
-
 navBarMenuItems.forEach((item) => {
   item.addEventListener('click', () => {
-    closeNav()
+    closeNav();
   })
 });
 
-
-hamburgerMenu.addEventListener('click', function() {
-  
+hamburgerMenu.addEventListener('click', () =>  {
     const isNavOpen = navbarMenu.classList.contains("open");
     if (!isNavOpen) {
       openNav();
