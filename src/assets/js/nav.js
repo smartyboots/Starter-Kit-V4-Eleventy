cef02f92b63fccbf4efb256efdf5880f4d25b9f6
Lala H <smartyboots@users.noreply.github.com>
2024-10-09 13:34:57 +1300

    nav updates
    mobile nav now closes on click

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -2,17 +2,34 @@
 //    Toggle Mobile Navigation
 //
 const navbarMenu = document.querySelector("#navigation #navbar-menu");
+const navBarMenuItems = navbarMenu.querySelectorAll("ul li a");
 const hamburgerMenu = document.querySelector("#navigation .hamburger-menu");
 
+function openNav() {
+  hamburgerMenu.setAttribute("aria-expanded", true);
+  hamburgerMenu.classList.add("clicked");
+  navbarMenu.classList.add("open");
+}
+function closeNav() {
+  hamburgerMenu.setAttribute("aria-expanded", false);
+    hamburgerMenu.classList.remove("clicked");
+    navbarMenu.classList.remove("open");
+}
+
+
+navBarMenuItems.forEach((item) => {
+  item.addEventListener('click', () => {
+    closeNav()
+  })
+});
+
+
 hamburgerMenu.addEventListener('click', function() {
+  
     const isNavOpen = navbarMenu.classList.contains("open");
     if (!isNavOpen) {
-        hamburgerMenu.setAttribute("aria-expanded", true);
-        hamburgerMenu.classList.add("clicked");
-        navbarMenu.classList.add("open");
+      openNav();
     } else {
-        hamburgerMenu.setAttribute("aria-expanded", false);
-        hamburgerMenu.classList.remove("clicked");
-        navbarMenu.classList.remove("open");
+      closeNav(); 
     }
 });
